a6851868256612bc660c9bc1407ca8eba4481ecb
eusekerci <enesugursekerci@gmail.com>
2017-06-04 17:15:09 +0300

    uniRX and MessageBus

---
+++ Assets/Scripts/Utility/MessageBus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PistiClub
+{
+    public abstract class PcEvent
+    {
+
+    }
+
+    public static class MessageBus
+    {
+        public static void Publish<T>(T evnt) where T : PcEvent
+        {
+            UniRx.MessageBroker.Default.Publish(evnt);
+        }
+
+        public static UniRx.IObservable<T> OnEvent<T>() where T : PcEvent
+        {
+            return UniRx.MessageBroker.Default.Receive<T>();
+        }
+    }
+}
